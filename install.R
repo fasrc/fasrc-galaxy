7a053a53cbabaf7af7c4b4ea6f3b274a337a7e81
nate <n8@adullmoment.com>
2016-04-06 12:48:10 -0400

    add R dependencies

--- install.R
+++ install.R
@@ -1,2 +1,2 @@
-install.packages(c("modeltools","coin","agricolae","gamlss","inlinedocs","logging","optparse","outliers","penalized"), repos = "http://cran.case.edu")
+install.packages(c("modeltools","testthat","coin","agricolae","gam","gbm,","glmnet","gamlss","mlinedocs","inlinedocs","logging","optparse","outliers","penalized"), repos = "http://cran.case.edu")
 installed.packages()
