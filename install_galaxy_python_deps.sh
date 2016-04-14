1aef848360452f5dc442580112fc53e33f518ed9
nate <n8@adullmoment.com>
2016-04-14 12:30:21 -0400

    add biopython dependency

--- install_galaxy_python_deps.sh
+++ install_galaxy_python_deps.sh
@@ -2,4 +2,4 @@
 export GALAXY_VIRTUAL_ENV=/galaxy_venv
 source $GALAXY_VIRTUAL_ENV/bin/activate && \
 pip install setuptools --upgrade && \
-pip install -v psutil scipy numpy rpy2 matplotlib blist biom-format h5py cogent mlpy mpi4py
+pip install -v psutil scipy numpy rpy2 matplotlib biopython blist biom-format h5py cogent mlpy mpi4py
