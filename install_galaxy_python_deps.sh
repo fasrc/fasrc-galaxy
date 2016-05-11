f786010db95bf113c399ca8b0018a6b3bdcc4d8a
nate <n8@adullmoment.com>
2016-05-11 10:49:51 -0400

    nstall biom-format version 1.0.0 only

--- install_galaxy_python_deps.sh
+++ install_galaxy_python_deps.sh
@@ -2,4 +2,5 @@
 export GALAXY_VIRTUAL_ENV=/galaxy_venv
 source $GALAXY_VIRTUAL_ENV/bin/activate && \
 pip install setuptools --upgrade && \
-pip install -v psutil scipy numpy rpy2 matplotlib biopython blist biom-format h5py cogent mlpy mpi4py
+pip install -v psutil scipy numpy rpy2 matplotlib biopython blist h5py cogent mlpy mpi4py
+pip install -v biom-format==1.0.0
