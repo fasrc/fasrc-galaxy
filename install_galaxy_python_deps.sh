221a37c810102a48918db0b51f3aee80db0396b1
nate <n8@adullmoment.com>
2016-05-11 19:45:39 -0400

    looks like the oldest available is 1.3.0. use it.

--- install_galaxy_python_deps.sh
+++ install_galaxy_python_deps.sh
@@ -3,4 +3,4 @@ export GALAXY_VIRTUAL_ENV=/galaxy_venv
 source $GALAXY_VIRTUAL_ENV/bin/activate && \
 pip install setuptools --upgrade && \
 pip install -v psutil scipy numpy rpy2 matplotlib biopython blist h5py cogent mlpy mpi4py
-pip install -v biom-format==1.0.0
+pip install -v biom-format==1.3.0
