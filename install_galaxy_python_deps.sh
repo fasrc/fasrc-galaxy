17ef7f544adca59453510496dc365c1a18b96add
nate <n8@adullmoment.com>
2016-05-28 22:19:17 -0400

    add sqlalchemy to the python deps

--- install_galaxy_python_deps.sh
+++ install_galaxy_python_deps.sh
@@ -2,5 +2,5 @@
 export GALAXY_VIRTUAL_ENV=/galaxy_venv
 source $GALAXY_VIRTUAL_ENV/bin/activate && \
 pip install setuptools --upgrade && \
-pip install -v psutil scipy numpy rpy2 matplotlib biopython blist h5py cogent mlpy mpi4py
+pip install -v psutil scipy numpy rpy2 matplotlib biopython blist h5py cogent mlpy mpi4py sqlalchemy
 pip install -v biom-format==1.3.0
